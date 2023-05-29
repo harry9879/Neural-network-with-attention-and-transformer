29b55f00c1d9c2f9a6ec6b37471bdfdea3e957d4
Harmandeep singh <114888977+harry9879@users.noreply.github.com>
2023-05-29 23:20:16 +0530

    Delete practice set.py

--- practice set.py
+++
@@ -1,9 +0,0 @@
-letter='''dear <|NAME|>
-I am happy to tell you that you are selected 
-<|DATE|>
-'''
-a=input('Enter your name \n')
-b=input('Enter your date \n')
-letter= letter.replace('<|NAME|>',a)
-letter= letter.replace('<|DATE|>',b)
-print(letter)
